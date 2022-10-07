cc013144b9a7077fe4a91d3c5e24ccbba3eaae35
maxwell1700 <43969904+maxwell1700@users.noreply.github.com>
2022-10-07 10:39:54 +0100

    added feature branch change

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,3 +1,16 @@
 public class FizzBuzz {
 
+    public static String fizzBuzz(int val)
+    {
+        return val % 5 == 0 && val % 3 == 0 ? "fizzbuzz" : null;
+    }
+
+    public String fizz(int val)
+    {
+        return val % 5 == 0 ? "fizz" : null;
+    }
+    public String buzz(int val)
+    {
+        return val % 3 == 0 ? "buzz" : null;
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FizzBuzzTest {
+
+
+    @Test
+    public void fizzBuzzTest(){
+        Assert.assertEquals("fizzbuzz", FizzBuzz.fizzBuzz(15));
+    }
 }
